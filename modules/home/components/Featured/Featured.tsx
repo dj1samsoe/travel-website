f740044dcc2bb4a09481df876695ec69dd23b1e7
dj1samsoe <dhanyh86@gmail.com>
2024-01-31 20:55:09 +0700

    feat: Add Featured Section

---
+++ modules/home/components/Featured/Featured.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Featured() {
+  return (
+    <section className="flex flex-col items-center justify-center px-16 py-20">
+      <h1 className="text-xl font-bold uppercase">Also Featured In</h1>
+      <div className="w-full overflow-x-scroll no-scrollbar py-5">
+        <div className="flex gap-10">
+          <Image
+            src="/featured-1.png"
+            alt="featured-logo"
+            width={300}
+            height={100}
+          />
+          <Image
+            src="/featured-2.png"
+            alt="featured-logo"
+            width={150}
+            height={100}
+          />
+          <Image
+            src="/featured-3.png"
+            alt="featured-logo"
+            width={150}
+            height={100}
+          />
+          <Image
+            src="/featured-4.png"
+            alt="featured-logo"
+            width={150}
+            height={100}
+          />
+          <Image
+            src="/featured-5.png"
+            alt="featured-logo"
+            width={300}
+            height={100}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
--- modules/home/components/Home.tsx
+++ modules/home/components/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero/Hero";
+import Featured from "./Featured/Featured";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <Featured />
     </>
   );
 }
