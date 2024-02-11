c037ed9ba7658be4a33a71267c7b362d33fec3fa
dj1samsoe <dhanyh86@gmail.com>
2024-02-11 23:27:45 +0700

    style: change responsive button

--- modules/home/components/Stories/Stories.tsx
+++ modules/home/components/Stories/Stories.tsx
@@ -1,12 +1,15 @@
+"use client";
 import { STORIES } from "@/common/constant/stories";
 import React from "react";
 import StoriesCard from "./StoriesCard";
 import { Button } from "@chakra-ui/react";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import useIsMobile from "@/common/hooks/useIsMobile";
 
 export default function Stories() {
+  const isMobile = useIsMobile();
   return (
-    <section className="flex flex-col items-center justify-center md:px-16 px-5 pt-20 pb-10">
+    <section className="flex flex-col items-center justify-center md:px-16 px-5 pt-20 pb-20">
       <div className="flex w-full items-center justify-between pb-10">
         <div className="flex flex-col space-y-3 pb-5">
           <h1 className="text-3xl font-bold">Top Travel Stories</h1>
@@ -15,15 +18,22 @@ export default function Stories() {
           </p>
         </div>
 
-        <Button colorScheme="blue" variant="outline" rounded={"full"}>
-          View All Stories
-        </Button>
+        {isMobile ? null : (
+          <Button colorScheme="blue" variant="outline" rounded={"full"}>
+            View All Stories
+          </Button>
+        )}
       </div>
-      <div className="grid sm:grid-cols-2 grid-cols-1 gap-7">
+      <div className="grid sm:grid-cols-2 grid-cols-1 gap-7 pb-10">
         {STORIES.map((items, index) => (
           <StoriesCard key={index} {...items} />
         ))}
       </div>
+      {isMobile && (
+        <Button colorScheme="blue" variant="outline" rounded={"full"}>
+          View All Stories
+        </Button>
+      )}
       <div className="flex items-center justify-center pt-10">
         <div className="flex items-center gap-3">
           <IoIosArrowBack />
